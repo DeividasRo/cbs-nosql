c9e977b20fd6dbfa8b6d1d9c2a76bd36881fa2a3
DeividasRo <slenderiss@gmail.com>
2023-09-17 15:01:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import redis
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+
+
+
+
+
+def main():
+    print("Welcome to the Cinema Booking System!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# https://redis.io/docs/clients/python/
+
