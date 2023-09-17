d3a42867241f7740be9262218c6f7bba38a7a974
Pauliuses <100035297+Pauliuses@users.noreply.github.com>
2023-09-17 16:50:17 +0300

    Update Cinema_nosql.py
    Fixes

--- Cinema_nosql.py
+++ Cinema_nosql.py
@@ -1,4 +1,6 @@
 import redis
+import json
+from datetime import datetime
 
 r = redis.Redis(host='localhost', port=6379)
 
@@ -17,19 +19,20 @@ def get_screening(session_id):
     else:
         return None
 
-
 if __name__ == "__main__":
     sessions = [
-        {"session_id": "session1", "session_date": "2023-09-18", "session_time": "15:00", "movie_title": "Movie Title 1"},
-        {"session_id": "session2", "session_date": "2023-09-19", "session_time": "18:00", "movie_title": "Movie Title 2"},
+        {"session_id": "session123", "session_date": "2023-09-18", "session_time": "15:00", "movie_title": "Movie Title 1"},
+        {"session_id": "session456", "session_date": "2023-09-19", "session_time": "17:30", "movie_title": "Movie Title 2"}
     ]
 
     for session in sessions:
-        add_screening(session["session_id"], session["session_date"], session["session_time"], session["movie_title"])
+        add_screening(session['session_id'], session['session_date'], session['session_time'], session['movie_title'])
 
     for session in sessions:
-        screening_details = get_screening(session["session_id"])
+        screening_details = get_screening(session['session_id'])
         if screening_details:
             print()
             for key, value in screening_details.items():
                 print(f"{key}: {value}")
+        else:
+            print(f"No screening found for session {session['session_id']}.")
